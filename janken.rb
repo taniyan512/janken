7d7fd106a0565afc0013c151050315902d2e4dce
EC2 Default User <ec2-user@ip-172-31-16-175.ec2.internal>
2022-03-30 12:19:02 +0000

    じゃんけん

--- janken.rb
+++ janken.rb
@@ -25,48 +25,48 @@ end
 
 #あなたが指を指す時のメソッド
 def yubiwo_sasu
-  puts "勝ちまであと少し"
-  puts "あっち向いて〜"
-  puts "0(上)1(下)2(左)3(右)"
+ puts "勝ちまであと少し"
+ puts "あっち向いて〜"
+ puts "0(上)1(下)2(左)3(右)"
   
-  player_yubi = gets.to_i
-  program_kao = rand(3)
+ player_yubi = gets.to_i
+ program_kao = rand(3)
   
-  puts "ホイ！"
+ puts "ホイ！"
   
-  directions = ["上","下","左","右"]
+ directions = ["上","下","左","右"]
   
-  puts  "あなた：#{directions[player_yubi]}"
-  puts  "プログラム：#{directions[program_kao]}"
+ puts  "あなた：#{directions[player_yubi]}"
+ puts  "プログラム：#{directions[program_kao]}"
   
-  if player_yubi == program_kao
-    puts "あなたの勝ちです！"
-  else
-    janken
-  end
+ if player_yubi == program_kao
+   puts "あなたの勝ちです！"
+ else
+   janken
+ end
 end
 
 #あなたが顔を向く時のメソッド
 def kaowo_muku
-  puts "負けるかも？"
-  puts "あっち向いて〜"
-  puts "0(上)1(下)2(左)3(右)"
+ puts "負けるかも？"
+ puts "あっち向いて〜"
+ puts "0(上)1(下)2(左)3(右)"
   
-  player_kao = gets.to_i
-  program_yubi = rand(3)
+ player_kao = gets.to_i
+ program_yubi = rand(3)
   
-  puts "ホイ！"
+ puts "ホイ！"
   
-    directions = ["上","下","左","右"]
+ directions = ["上","下","左","右"]
     
-  puts  "あなた：#{directions[player_kao]}"
-  puts  "プログラム：#{directions[program_yubi]}"
+ puts  "あなた：#{directions[player_kao]}"
+ puts  "プログラム：#{directions[program_yubi]}"
   
-  if player_kao == program_yubi
-    puts "あなたの負けです"
-  else
-    janken
-  end
+ if player_kao == program_yubi
+   puts "あなたの負けです"
+ else
+   janken
+ end
 
 end
  
